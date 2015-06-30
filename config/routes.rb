aecb09b2fa6ab2ae9cb9beb963797e847819ab40
Dimitar Kostov <dimitar.kostov@158ltd.com>
2015-06-30 15:57:11 +0300

    Controller Views Bootstrap Questions Answers Dimitar Kostov

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   get '/about' => 'home#about'
   get '/home' => 'home#index'
+  post 'questions' => 'home#temp'
+  post 'answers' => 'home#temp'
+  get 'question/:id' => 'home#question'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
